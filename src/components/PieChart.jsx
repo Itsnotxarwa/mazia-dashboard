92c6a6ee659a9546d050f521465a91416f5f0bdd
itsnotxarwa <marwa.kchkh@gmail.com>
2026-02-01 18:22:07 +0100

    add pie chart to represent calls ended by ai and those ended by human

---
+++ src/components/PieChart.jsx
@@ -0,0 +1,93 @@
+import { PieChart, Pie, Sector, Tooltip, Cell } from "recharts";
+
+const COLORS = ["#032ca6", "#a0b0e0"]; 
+
+const renderActiveShape = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+}) => {
+    const RADIAN = Math.PI / 180;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
+    
+    return (
+    <g>
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill} fontWeight="bold">
+            {payload.name}
+        </text>
+        
+        <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+        />
+        <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+        />
+        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">
+            {`Calls ${value}`}
+        </text>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+            {`(${(percent * 100).toFixed(1)}%)`}
+        </text>
+    </g>
+);
+};
+
+export default function CustomPieAIHuman({ agentEnded, humanEnded }) {
+    const data = [
+        { name: "AI", value: agentEnded },
+        { name: "Human", value: humanEnded },
+    ];
+    return (
+    <div className=" text-black bg-white p-4 rounded-lg shadow-md">
+        <h3 className="text-2xl text-center tracking-tight">Répartition AI / Humain</h3>
+        <PieChart width="100%" height={300}>
+            <Pie
+                activeIndex={0}
+                activeShape={renderActiveShape}
+                data={data}
+                cx="50%"
+                cy="50%"
+                innerRadius={80}
+                outerRadius={120}
+                fill="#8884d8"
+                dataKey="value"
+            >
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index]} />
+                ))}
+            </Pie>
+            <Tooltip />
+        </PieChart>
+    </div>
+);
+}
