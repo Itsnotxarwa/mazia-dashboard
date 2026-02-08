8d0036334c00aaf3dc03564cb8a6b0296cfcadc4
itsnotxarwa <marwa.kchkh@gmail.com>
2026-02-08 20:38:12 +0100

    add overview

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import CardsData from "../data/cardsData";
 import Sidebar from "./sidebar";
-import KpiCards from "./kpiCards";
-import PieChartAIHuman from "./PieChart";
-import CallsBarChart from "./CallsBarChart";
+import Overview from "./overview";
+
 
 export default function Dashboard() {
     const [overview, setOverview] = useState(null);
@@ -23,15 +22,8 @@ export default function Dashboard() {
     return(
         <div className="flex min-h-screen bg-white text-black">
             <Sidebar />
-
             <main className="bg-gray-100 flex-1">
-                <div className="flex-1 mt-4 mx-3 md:mx-8">
-                <KpiCards cards={cards} />
-                <div className="mt-8 flex flex-col lg:flex-row items-center gap-4 w-full justify-between">
-                    <CallsBarChart last7Days={overview.volume.calls_last_7_days} last30Days={overview.volume.calls_last_30_days} />
-                    <PieChartAIHuman agentEnded={overview.termination.agent_ended} humanEnded={overview.termination.human_ended} />
-                </div>
-                </div>
+                <Overview cards={cards} overview={overview} />
             </main>
 
         </div>
