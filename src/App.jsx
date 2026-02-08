af9f7942236372b720d3dd712e5b2447c16e9300
itsnotxarwa <marwa.kchkh@gmail.com>
2026-02-08 17:18:02 +0100

    add history route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard';
+import History from './components/history';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Router>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/history" element={<History />} />
       </Routes>
     </Router>
     </div>
