bd4511d6b1fd331586e9f4dea3643b7a6fc80293
itsnotxarwa <marwa.kchkh@gmail.com>
2026-02-10 19:17:11 +0100

    Update wording for French translation

--- src/components/callDetails.jsx
+++ src/components/callDetails.jsx
@@ -12,7 +12,7 @@ export default function CallDetails({ selectedCall, setSelectedCall }) {
     };
     return (
     <div className="fixed inset-0 bg-black/40 z-50 p-0.5">
-        <div className="bg-white rounded-lg min-h-screen lg:ml-68 p-14">
+        <div className="bg-white rounded-lg h-screen mb-4 lg:ml-200 p-14 overflow-y-auto relative">
             <h2 className="font-bold text-lg mb-3">
                 Conversation
             </h2>
@@ -21,32 +21,42 @@ export default function CallDetails({ selectedCall, setSelectedCall }) {
             </p>
 
             
-                {parsedTranscription.map((item, index) => (
-                    <div className={`space-y-2 flex flex-col max-w-xl mx-auto mt-4
-                    ${item.role === "user" ? "items-start" : "items-end"}`}>
-                    <div
+                {parsedTranscription && parsedTranscription.length > 0 ? (
+                    parsedTranscription.map((item, index) => (
+                    <div 
                     key={index}
-                    className={`p-2 rounded w-115 ${
+                    className={`space-y-2 flex flex-col max-w-xl mx-auto mt-4
+                    ${item.role === "user" ? "items-start" : "items-end"}`}>
+                        <div
+                        className={`p-2 rounded w-115 ${
                         item.role === "user"
                         ? "bg-gray-100 text-left"
                         : "bg-blue-50 text-left"
-                    }`}
-                    >
-                        <span className="font-semibold">
-                            {item.role === "user" ? "Client" : "Agent"}:
-                        </span>{" "}
-                        {item.content}
+                        }`}
+                        >
+                            <span className="font-semibold">
+                                {item.role === "user" ? "Client" : "Agent"}:
+                            </span>{" "}
+                            {item.content}
+                        </div>
                     </div>
-                    </div>
-                ))}
+                    ))
+                ) : (
+                    <p className="text-center text-gray-500 mt-6">
+                        Aucune transcription disponible
+                    </p>
+                )}
+            
+            <div className="h-px w-full bg-[#032ca6] my-8"></div>
 
-            <div>
-                <p><b>Date:</b> {formatDate(selectedCall.created_at)}</p>
-                <p><b>Durée:</b> {formatDuration(selectedCall.duration_seconds)}</p>
-                <p><b>De:</b> {selectedCall.from_number}</p>
-                <p><b>À:</b> {selectedCall.to_number}</p>
-                <p><b>Statut:</b> {selectedCall.call_status}</p>
-                <p><b>Raison:</b> {selectedCall.disconnect_reason}</p>
+            <div className="space-y-1">
+                <p className="flex justify-between font-semibold"><span className="text-[#032ca6] text-md font-semibold">Date:</span> {formatDate(selectedCall.created_at)}</p>
+                <p className="flex justify-between font-semibold"><span className="text-[#032ca6] text-md font-semibold">Durée:</span> {formatDuration(selectedCall.duration_seconds)}</p>
+                <p className="flex justify-between font-semibold"><span className="text-[#032ca6] text-md font-semibold">De:</span> {selectedCall.from_number}</p>
+                <p className="flex justify-between font-semibold"><span className="text-[#032ca6] text-md font-semibold">À:</span> {selectedCall.to_number}</p>
+                <p className="flex justify-between font-semibold"><span className="text-[#032ca6] text-md font-semibold">Statut:</span> {selectedCall.call_status === "ANSWERED" ? "Répondu" : "Manqué"}</p>
+                <p className="flex justify-between font-semibold"><span className="text-[#032ca6] text-md font-semibold">Raison:</span> {selectedCall.disconnect_reason === "HANGUP_BY_CALLEE" ? "Raccroché par le destinataire" : "Raccroché par l’agent"}</p>
+                <p className="flex justify-between font-semibold"><span className="text-[#032ca6] text-md font-semibold">Type d’appel:</span>{selectedCall.call_type === "inbound" ? "Entrant" : selectedCall.call_type === "outbound" ? "Sortant" : selectedCall.call_type}</p>
             </div>
             
             <button
