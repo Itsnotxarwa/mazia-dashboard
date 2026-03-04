80d23d1b10695714e5d9b1fa0e32e05e0985c5bb
itsnotxarwa <marwa.kchkh@gmail.com>
2026-03-04 16:18:06 +0100

    add login page with dashboard redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard';
 import History from './components/history';
+import Login from './Login';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
 
@@ -8,7 +10,8 @@ function App() {
     <div>
       <Router>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/history" element={<History />} />
       </Routes>
     </Router>
